bca950144ad7616c462494cc8eb2b37cc611128e
Natasha Johnson <njohnson533@gmail.com>
2023-04-21 16:18:03 -0400

    chore: add shape count

--- sketch.js
+++ sketch.js
@@ -12,50 +12,76 @@ function setup() {
 
   //side panel
   let sidePanel = createDiv();
-  sidePanel.style("position", "absolute");
+  sidePanel.style("position", "fixed");
   sidePanel.style("top", "0");
   sidePanel.style("left", "0");
   sidePanel.style("width", "200px");
-  sidePanel.style("height", windowHeight + "px");
+  sidePanel.style("height", window.innerHeight + "px");
   sidePanel.style("background-color", "#ccc");
   sidePanel.style("z-index", "1");
 
-  // stroke width slider
-  strokeWidthSlider = createSlider(0, 20, weight, 1);
-  strokeWidthSlider.parent(sidePanel);
-  strokeWidthSlider.position(10, 50);
-  strokeWidthSlider.style("width", "125px");
-  strokeWidthSlider.style("z-index", "2");
-
-  //stroke width label
-  let velocityLabel = createP("Velocity:");
-  velocityLabel.style("position", "absolute");
-  velocityLabel.style("top", "70px");
-  velocityLabel.style("left", "10px");
-  velocityLabel.style("font-size", "18px");
-  velocityLabel.style("color", "#000");
-  velocityLabel.style("z-index", "3");
-
-  // move slider
-  velocitySlider = createSlider(0, 20, velocity, 1);
-  velocitySlider.parent(sidePanel);
-  velocitySlider.position(10, 110);
-  velocitySlider.style("width", "125px");
-  velocitySlider.style("z-index", "2");
+  // directions label
+  let directions = createP("*Mouseover changes fill*");
+  directions.style("position", "absolute");
+  directions.style("top", "10px");
+  directions.style("left", "10px");
+  directions.style("font-size", "18px");
+  directions.style("color", "#000");
+  directions.style("z-index", "3");
+  directions.style("background-color", "yellow");
 
   //stroke width label
   let strokeWidthLabel = createP("Stroke Width:");
   strokeWidthLabel.style("position", "absolute");
-  strokeWidthLabel.style("top", "10px");
+  strokeWidthLabel.style("top", "50px");
   strokeWidthLabel.style("left", "10px");
   strokeWidthLabel.style("font-size", "18px");
   strokeWidthLabel.style("color", "#000");
   strokeWidthLabel.style("z-index", "3");
 
+  // stroke width slider
+  strokeWidthSlider = createSlider(0, 20, weight, 1);
+  strokeWidthSlider.parent(sidePanel);
+  strokeWidthSlider.position(10, 90);
+  strokeWidthSlider.style("width", "175px");
+  strokeWidthSlider.style("z-index", "2");
+
+  //shape count label
+  let shapeCountLabel = createP("Shape Count:");
+  shapeCountLabel.style("position", "absolute");
+  shapeCountLabel.style("top", "110px");
+  shapeCountLabel.style("left", "10px");
+  shapeCountLabel.style("font-size", "18px");
+  shapeCountLabel.style("color", "#000");
+  shapeCountLabel.style("z-index", "3");
+
+  // shape count slider
+  shapeCountSlider = createSlider(0, 50, count, 1);
+  shapeCountSlider.parent(sidePanel);
+  shapeCountSlider.position(10, 150);
+  shapeCountSlider.style("width", "175px");
+  shapeCountSlider.style("z-index", "2");
+
+  //velocity slider
+  let velocityLabel = createP("Velocity:");
+  velocityLabel.style("position", "absolute");
+  velocityLabel.style("top", "170px");
+  velocityLabel.style("left", "10px");
+  velocityLabel.style("font-size", "18px");
+  velocityLabel.style("color", "#000");
+  velocityLabel.style("z-index", "3");
+
+  // velocity slider label
+  velocitySlider = createSlider(0, 20, velocity, 1);
+  velocitySlider.parent(sidePanel);
+  velocitySlider.position(10, 210);
+  velocitySlider.style("width", "175px");
+  velocitySlider.style("z-index", "2");
+
   // shape label
   let shape = createP("Choose Shape:");
   shape.style("position", "absolute");
-  shape.style("top", "120px");
+  shape.style("top", "240px");
   shape.style("left", "10px");
   shape.style("font-size", "18px");
   shape.style("color", "#000");
@@ -64,35 +90,32 @@ function setup() {
   //checkboxes
   circle = createCheckbox("Circle", true);
   circle.changed(clearInactiveCheckbox.bind(circle));
-  circle.position(10, 170);
+  circle.position(10, 290);
   circle.style("z-index", "3");
 
   square = createCheckbox("Square", false);
   square.changed(clearInactiveCheckbox.bind(square));
-  square.position(10, 200);
+  square.position(10, 320);
   square.style("z-index", "3");
 
   tri = createCheckbox("Triangle", false);
   tri.changed(clearInactiveCheckbox.bind(tri));
-  tri.position(10, 230);
+  tri.position(10, 350);
   tri.style("z-index", "3");
 
   //reset canvas button
   button = createButton("Reset Canvas");
-  button.position(40, 275);
+  button.position(40, 400);
   button.style("z-index", "3");
   button.mousePressed(refillShapesArray);
+}
 
-  // shape label
-  let directions = createP("*Mouseover changes fill!");
-  directions.style("position", "absolute");
-  directions.style("top", "320px");
-  directions.style("left", "10px");
-  directions.style("font-size", "18px");
-  directions.style("color", "#000");
-  directions.style("z-index", "3");
+function draw() {
+  background(0);
+  weight = strokeWidthSlider.value();
+  velocity = velocitySlider.value();
+  count = shapeCountSlider.value();
 
-  //create and add new shape to shapes array
   for (let i = 0; i < count; i++) {
     let x = random(width);
     let y = random(height);
@@ -100,14 +123,9 @@ function setup() {
     let b = new Shape(x, y, r);
     shapes.push(b);
   }
-}
 
-function draw() {
-  background(0);
-  weight = strokeWidthSlider.value();
-  velocity = velocitySlider.value();
   //check if you have moused over a shape
-  for (let i = 0; i < shapes.length; i++) {
+  for (let i = 0; i < count; i++) {
     if (shapes[i].contains(mouseX, mouseY)) {
       shapes[i].changeColor();
     }
@@ -119,7 +137,9 @@ function draw() {
 
 //add back missing number of shapes
 function refillShapesArray() {
-  for (let i = 0; i < count - shapes.length; i++) {
+  background(0);
+  shapes = [];
+  for (let i = 0; i < count; i++) {
     let x = random(width);
     let y = random(height);
     let r = random(20, 60);
@@ -208,12 +228,12 @@ class Shape {
     }
     if (tri.checked()) {
       triangle(
-        this.x - (1.75 * this.r),
-        this.y + (1.75 * this.r),
-        this.x + (1.75 * this.r),
-        this.y + (1.75 * this.r),
+        this.x - 1.75 * this.r,
+        this.y + 1.75 * this.r,
+        this.x + 1.75 * this.r,
+        this.y + 1.75 * this.r,
         this.x,
-        this.y - (1.75 * this.r)
+        this.y - 1.75 * this.r
       );
     }
   }
